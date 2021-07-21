6d46cc1b90724545098592f62a51a1937dc6cade
gio broliatto <84979354+giobroliatto@users.noreply.github.com>
2021-07-21 11:07:52 -0300

    update script.js
    digital clock color changing system working

--- project 02/script.js
+++ project 02/script.js
@@ -10,9 +10,11 @@ function updateClock() {
   const hours = now.getHours()
 
   // fix the times that are < 10, which has only 2 characters by default
-  digitalElement.innerHTML = `${fixZero(hours)}:${fixZero(minutes)}:${fixZero(
-    seconds
-  )}`
+  digitalElement.innerHTML = `<span class="hr">${fixZero(
+    hours
+  )}</span>:<span class="min">${fixZero(
+    minutes
+  )}</span>:<span class="sec">${fixZero(seconds)}</span>`
 
   // fix the pointers positions
   const sDeg = (360 / 60) * seconds + 270 // (360 deg in a circle)/(60 seconds) * seconds to set the position + 270 deg so the pointer start in 0
@@ -32,5 +34,42 @@ function fixZero(time) {
   }
 }
 
+function changeDigitalClockColor() {
+  const now = new Date()
+  const seconds = now.getSeconds()
+  const minutes = now.getMinutes()
+
+  const secColor = digitalElement.querySelector('.sec')
+  secColor.classList.add('color')
+  secColor.classList.remove('std-color')
+  setTimeout(() => {
+    secColor.classList.add('std-color')
+    secColor.classList.remove('color')
+  }, 250)
+
+  if (seconds == 0) {
+    console.log('rodei!')
+    const minColor = digitalElement.querySelector('.min')
+    minColor.classList.add('color')
+    minColor.classList.remove('std-color')
+    setTimeout(() => {
+      minColor.classList.add('std-color')
+      minColor.classList.remove('color')
+    }, 250)
+  }
+
+  if (minutes == 0 && seconds == 0) {
+    console.log('rodei também!')
+    const hrColor = digitalElement.querySelector('.hr')
+    hrColor.classList.add('color')
+    hrColor.classList.remove('std-color')
+    setTimeout(() => {
+      hrColor.classList.add('std-color')
+      hrColor.classList.remove('color')
+    }, 250)
+  }
+}
+
 setInterval(updateClock, 1000)
+setInterval(changeDigitalClockColor, 1000)
 updateClock()
